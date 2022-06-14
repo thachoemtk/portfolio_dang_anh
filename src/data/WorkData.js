9cc9add882e555b8075db8b27354e51fa86873e2
dang anh <danganh@bincg.com>
2022-06-14 20:16:45 +0700

    fix

---
+++ src/data/WorkData.js
@@ -0,0 +1,48 @@
+//Work Data
+export const Work = [
+    {
+        id:1,
+        name:"Travelner",
+        description:"As a smart application providing package travel solutions globally, Travelner app uses platform technology to provide travelers with an online travel network",
+        tags:["PHP","Note JS"],
+        
+        demo:"https://www.travelner.com",
+        icon:"/photo/travelner.ico"
+    },
+    {
+        id:2,
+        name:"PAP mall",
+        description:"Papmall was founded in June 2019 as an international e-commerce platform that provides online shoppers a simple, secure, and enjoyable online shopping experience and plays as a solution for global vendors to optimize sales revenue.",
+        tags:["PHP/Phalcon","JS"],
+        
+        demo:"https://www.papmall.com/",
+        icon:"/photo/papmall.ico"        
+    },
+    {
+        id:3,
+        name:"Forex CEC",
+        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+        tags:["PHP","Phalcon","JS"],
+        
+        demo:"https://www.forexcec.com",
+        icon:"/photo/forexcec.ico"    },
+    {
+        id:4,
+        name:"DNBC Financial Group",
+        description:"Ranking in the top list of the most high-class financial service providers in Europe, DNBCnet provides efficient services of opening all-in-one current IBAN accounts.",
+        tags:["PHP","Phalcon","JS"],
+        
+        demo:"https://www.dnbcf.com",
+        icon:"/photo/dnbcf.ico"       
+    },{
+        id:5,
+        name:"Khai Hung Land",
+        description:"The first trusted real estate service brand in Vietnam.",
+        tags:["PHP","Phalcon","JS"],
+        
+        demo:"http://khaihungland.com.vn",
+        icon:"/photo/khaihung.ico"        
+    }
+  
+  
+] 
